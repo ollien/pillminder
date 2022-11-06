1421e4b1818dba71279ecc80fee79f6fd494dead
Nick Krichevsky <nick@ollien.com>
2022-11-06 18:21:50 -0500

    Time out at each interval

--- lib/pillminder.ex
+++ lib/pillminder.ex
@@ -4,7 +4,10 @@ defmodule Pillminder do
   @spec send_reminders(non_neg_integer) :: :ok | {:error, any}
   def send_reminders(interval) do
     timer_start_res =
-      RunInterval.apply_interval(interval, &Pillminder.ReminderServer.send_reminder/0)
+      RunInterval.apply_interval(interval, fn ->
+        # We want to time out the call once our next interval hits
+        Pillminder.ReminderServer.send_reminder(interval)
+      end)
 
     case timer_start_res do
       {:ok, _} -> :ok
