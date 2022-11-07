f6bd6479539760a7da1067865e88d45dc98a90bb
Nick Krichevsky <nick@ollien.com>
2022-11-06 21:21:37 -0500

    Cut send_reminders over to use new function on reminder_server

--- lib/pillminder.ex
+++ lib/pillminder.ex
@@ -1,15 +1,9 @@
 defmodule Pillminder do
-  alias Pillminder.RunInterval
+  alias Pillminder.ReminderServer
 
   @spec send_reminders(non_neg_integer) :: :ok | {:error, any}
   def send_reminders(interval) do
-    timer_start_res =
-      RunInterval.apply_interval(interval, fn ->
-        # We want to time out the call once our next interval hits
-        Pillminder.ReminderServer.send_reminder(interval)
-      end)
-
-    case timer_start_res do
+    case ReminderServer.send_reminder_on_interval(interval) do
       {:ok, _} -> :ok
       {:error, err} -> {:error, err}
     end
