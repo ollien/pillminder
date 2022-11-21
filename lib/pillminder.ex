08f8f8d92e3988d1c8df123ebe341bd85307cc5e
Nick Krichevsky <nick@ollien.com>
2022-11-21 15:35:47 -0500

    Remove TODO that is no longer needed

--- lib/pillminder.ex
+++ lib/pillminder.ex
@@ -5,7 +5,6 @@ defmodule Pillminder do
   @spec send_reminder_for_timer(Config.Timer) :: :ok | {:error, any}
   def send_reminder_for_timer(timer) do
     ReminderServer.send_reminder_on_interval(timer.reminder_spacing,
-      # TODO: We should find a way to nomalize this a bit, rather than repeating what we did in the application config
       server_name: Pillminder.Application.reminder_server_via_tuple(timer)
     )
   end
