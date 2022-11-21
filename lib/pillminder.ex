95d78929b32a85243402edfcfcfddd5e65eade1a
Nick Krichevsky <nick@ollien.com>
2022-11-21 11:35:16 -0500

    Wire up scheduler to main supervisor

--- lib/pillminder.ex
+++ lib/pillminder.ex
@@ -2,21 +2,8 @@ defmodule Pillminder do
   alias Pillminder.ReminderServer
   alias Pillminder.Config
 
-  @spec send_reminders([Config.Timer]) :: :ok | {:error, any}
-  def send_reminders([]) do
-    :ok
-  end
-
-  def send_reminders([next_timer | rest]) do
-    with :ok <- send_reminder_for_timer(next_timer) do
-      send_reminders(rest)
-    else
-      err -> err
-    end
-  end
-
   @spec send_reminder_for_timer(Config.Timer) :: :ok | {:error, any}
-  defp send_reminder_for_timer(timer) do
+  def send_reminder_for_timer(timer) do
     ReminderServer.send_reminder_on_interval(timer.reminder_spacing,
       # TODO: We should find a way to nomalize this a bit, rather than repeating what we did in the application config
       server_name: Pillminder.Application.reminder_server_via_tuple(timer)
--- lib/pillminder/application.ex
+++ lib/pillminder/application.ex
@@ -1,4 +1,7 @@
 defmodule Pillminder.Application do
+  require Logger
+
+  alias Pillminder.Scheduler
   alias Pillminder.ReminderServer
   alias Pillminder.Config
 
@@ -9,22 +12,18 @@ defmodule Pillminder.Application do
   @impl true
   def start(_type, _args) do
     timers = Config.load_timers_from_env!()
-    timer_specs = Enum.map(timers, &make_spec_for_timer/1)
+    timer_specs = Enum.map(timers, &make_reminder_server_spec/1)
 
     children =
       [{Registry, keys: :unique, name: @registry_name}] ++
         timer_specs ++
         [
+          {Scheduler, make_scheduler_args(timers)},
           # # TODO: Add port to config file
           {Plug.Cowboy, scheme: :http, plug: Pillminder.WebRouter, options: [port: 8000]}
         ]
 
-    {:ok, supervisor_pid} =
-      Supervisor.start_link(children, strategy: :one_for_one, name: Pillminder.Supervisor)
-
-    :ok = Pillminder.send_reminders(timers)
-
-    {:ok, supervisor_pid}
+    Supervisor.start_link(children, strategy: :one_for_one, name: Pillminder.Supervisor)
   end
 
   @spec reminder_server_registry() :: module
@@ -35,8 +34,8 @@ defmodule Pillminder.Application do
     {:via, Registry, {ReminderServer.Registry, make_reminder_server_id(timer)}}
   end
 
-  @spec make_spec_for_timer(Config.Timer) :: Supervisor.child_spec()
-  defp make_spec_for_timer(timer) do
+  @spec make_reminder_server_spec(Config.Timer) :: Supervisor.child_spec()
+  defp make_reminder_server_spec(timer) do
     Supervisor.child_spec(
       {
         ReminderServer,
@@ -48,6 +47,27 @@ defmodule Pillminder.Application do
       },
       id: make_reminder_server_id(timer)
     )
+  end
+
+  @spec make_scheduler_args([Config.Timer.t()]) ::
+          {[Scheduler.scheduled_reminder()], Scheduler.init_options()}
+  defp make_scheduler_args(timers) do
+    {
+      Enum.map(timers, &make_reminder_for_scheduler/1),
+      []
+    }
+  end
+
+  @spec make_reminder_for_scheduler(Config.Timer.t()) :: Scheduler.scheduled_reminder()
+  defp make_reminder_for_scheduler(timer) do
+    %{
+      start_time: timer.reminder_start_time,
+      scheduled_func: fn ->
+        # The task supervisor in the Scheduler should re-run this, so it's ok to assert
+        :ok = Pillminder.send_reminder_for_timer(timer)
+        Logger.debug("Sent reminder for timer starting at #{timer.reminder_start_time}")
+      end
+    }
   end
 
   @spec make_reminder_server_id(Config.Timer) :: String.t()
