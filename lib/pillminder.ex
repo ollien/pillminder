ec62c4afc8cb4b8bb913e8c9aaa1518892a4f17c
Nick Krichevsky <nick@ollien.com>
2022-11-22 01:22:35 -0500

    Use send_immediately in send_reminder_for_timer

--- lib/pillminder.ex
+++ lib/pillminder.ex
@@ -5,6 +5,7 @@ defmodule Pillminder do
   @spec send_reminder_for_timer(Config.Timer) :: :ok | {:error, any}
   def send_reminder_for_timer(timer) do
     ReminderServer.send_reminder_on_interval(timer.reminder_spacing,
+      send_immediately: true,
       server_name: Pillminder.Application.reminder_server_via_tuple(timer)
     )
   end
