b5081048aa6f8bc9b6b94959c03a407f89eb5b9f
Nick Krichevsky <nick@ollien.com>
2023-01-08 14:16:57 -0500

    Move colors to common dir

--- web-client/src/pages/login/Login.tsx
+++ web-client/src/pages/login/Login.tsx
@@ -6,14 +6,13 @@ import {
 	Container,
 	Heading,
 } from "@chakra-ui/react";
+import colors from "pillminder-webclient/src/pages/_common/colors";
 import LoginForm from "pillminder-webclient/src/pages/login/LoginForm";
 import * as React from "react";
 
-const BACKGROUND_COLOR = "#37474F";
-
 const Login = () => (
-	<Center h="100%" flexDirection="column" backgroundColor={BACKGROUND_COLOR}>
-		<Container paddingTop={2}>
+	<Center h="100%" flexDirection="column" backgroundColor={colors.BACKGROUND}>
+		<Container>
 			<Card backgroundColor="white" shadow="2xl">
 				<CardHeader>
 					<Heading size="md">Welcome to Pillminder</Heading>
