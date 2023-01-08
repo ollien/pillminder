09ef4a342fba6422d050a43756b41cdcd8675146
Nick Krichevsky <nick@ollien.com>
2023-01-07 21:46:41 -0500

    Add validation for login form

--- web-client/src/pages/login/LoginForm.tsx
+++ web-client/src/pages/login/LoginForm.tsx
@@ -1,33 +1,65 @@
-import React, { useState } from "react";
+import React from "react";
 import { BeatLoader } from "react-spinners";
-import { Button, Flex, FormControl, FormLabel, Input } from "@chakra-ui/react";
+import {
+	Button,
+	FormControl,
+	FormErrorMessage,
+	FormLabel,
+	Input,
+} from "@chakra-ui/react";
+import { Field, Form, Formik, FieldProps, FormikBag } from "formik";
+
+const validateRequired = (value: string) => {
+	if (value) {
+		return undefined;
+	}
+
+	return "Required field";
+};
+
+interface FormData {
+	pillminder: string;
+}
 
 const LoginForm = () => {
-	const [isLoading, setIsLoading] = useState<boolean>(false);
-	const submit = () => {
-		setIsLoading(true);
+	const submit = (values: FormData, { setSubmitting }) => {
+		console.log(values);
 	};
 
 	return (
-		<>
-			<FormControl>
-				<FormLabel>Pillminder name</FormLabel>
-				<Input placeholder="My Awesome Pillminder"></Input>
-			</FormControl>
-			<FormControl>
-				<Button
-					isLoading={isLoading}
-					onClick={submit}
-					spinner={<BeatLoader size={8} color="white" />}
-					width="100%"
-					marginTop={4}
-					type="submit"
-					colorScheme="teal"
-				>
-					Log In
-				</Button>
-			</FormControl>
-		</>
+		<Formik<FormData> initialValues={{ pillminder: "" }} onSubmit={submit}>
+			{(formik) => (
+				<Form>
+					<Field name="pillminder" validate={validateRequired}>
+						{({ field, form }: FieldProps) => (
+							<FormControl
+								isInvalid={
+									!!form.errors.pillminder && !!form.touched.pillminder
+								}
+							>
+								<FormLabel>Pillminder name</FormLabel>
+								<FormErrorMessage>
+									{form.errors.pillminder?.toString() ?? ""}
+								</FormErrorMessage>
+								<Input {...field} placeholder="My Awesome Pillminder"></Input>
+							</FormControl>
+						)}
+					</Field>
+					<FormControl>
+						<Button
+							isLoading={formik.isSubmitting}
+							spinner={<BeatLoader size={8} color="white" />}
+							width="100%"
+							marginTop={4}
+							type="submit"
+							colorScheme="teal"
+						>
+							Log In
+						</Button>
+					</FormControl>
+				</Form>
+			)}
+		</Formik>
 	);
 };
 
