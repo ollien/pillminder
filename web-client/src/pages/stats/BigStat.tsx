6e7f12eb8905006f25a4123b3e448431cc4e573f
Nick Krichevsky <nick@ollien.com>
2023-01-08 22:16:12 -0500

    Use negative margin instead of lower line height for big stat

--- web-client/src/pages/stats/BigStat.tsx
+++ web-client/src/pages/stats/BigStat.tsx
@@ -9,7 +9,7 @@ interface BigStatProps {
 const BigStat = ({ value, name }: BigStatProps) => {
 	return (
 		<Stack textAlign="center">
-			<Text fontSize={{ base: "5rem", lg: "4rem" }} lineHeight="0.5">
+			<Text fontSize={{ base: "5rem", lg: "4rem" }} marginBottom="-6">
 				{value}
 			</Text>
 			<Text fontSize="lg">{name}</Text>
