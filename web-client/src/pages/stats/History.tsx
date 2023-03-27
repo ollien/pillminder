98d6f1e405672476ed21add44dc1867c5455d21e
Nick Krichevsky <nick@ollien.com>
2023-03-26 21:38:10 -0400

    Consolidate History components into History file

--- web-client/src/pages/stats/History.tsx
+++ web-client/src/pages/stats/History.tsx
@@ -1,13 +1,63 @@
 import { List } from "@chakra-ui/react";
+import { As, ListIcon, ListItem } from "@chakra-ui/react";
 import { TakenDate } from "pillminder-webclient/src/lib/api";
-import HistoryListItem, {
-	Status as HistoryStatus,
-} from "pillminder-webclient/src/pages/stats/HistoryListItem";
+import { assertUnreachable } from "pillminder-webclient/src/pages/_common/errors";
 import React from "react";
+import {
+	AiOutlineCheckCircle,
+	AiOutlineCloseCircle,
+	AiOutlineHourglass,
+} from "react-icons/ai";
+
+enum Status {
+	TAKEN,
+	NOT_TAKEN,
+	NOT_TAKEN_YET,
+}
+
+interface HistoryListItemProps {
+	status: Status;
+	label: string;
+}
 
 interface HistoryProps {
 	takenDates: TakenDate[];
 }
+
+const colorForStatus = (status: Status): string => {
+	switch (status) {
+		case Status.TAKEN:
+			return "green";
+		case Status.NOT_TAKEN:
+			return "red";
+		case Status.NOT_TAKEN_YET:
+			return "gray";
+		default:
+			return assertUnreachable(status);
+	}
+};
+
+// The types for these icons are annoying to express, and not important other than that they are chakra.As
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const iconForStatus = (status: Status): As<any> => {
+	switch (status) {
+		case Status.TAKEN:
+			return AiOutlineCheckCircle;
+		case Status.NOT_TAKEN:
+			return AiOutlineCloseCircle;
+		case Status.NOT_TAKEN_YET:
+			return AiOutlineHourglass;
+		default:
+			return assertUnreachable(status);
+	}
+};
+
+const HistoryListItem = ({ status, label }: HistoryListItemProps) => (
+	<ListItem fontSize={{ base: "3xl", lg: "2xl" }}>
+		<ListIcon as={iconForStatus(status)} color={colorForStatus(status)} />
+		<span style={{ verticalAlign: "-0.125em" }}>{label}</span>
+	</ListItem>
+);
 
 const History = ({ takenDates }: HistoryProps) => {
 	const listItems = takenDates
@@ -19,11 +69,11 @@ const History = ({ takenDates }: HistoryProps) => {
 			});
 			const status = (() => {
 				if (idx == 0 && !takenDate.taken) {
-					return HistoryStatus.NOT_TAKEN_YET;
+					return Status.NOT_TAKEN_YET;
 				} else if (!takenDate.taken) {
-					return HistoryStatus.NOT_TAKEN;
+					return Status.NOT_TAKEN;
 				} else {
-					return HistoryStatus.TAKEN;
+					return Status.TAKEN;
 				}
 			})();
 
