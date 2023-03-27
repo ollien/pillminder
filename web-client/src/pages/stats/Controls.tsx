e9c23002f7be44fb046f32144f6130321f20be1f
Nick Krichevsky <nick@ollien.com>
2023-03-26 22:10:42 -0400

    Add dialog box to confirm skipping

--- web-client/src/pages/stats/Controls.tsx
+++ web-client/src/pages/stats/Controls.tsx
@@ -1,6 +1,17 @@
 import { CheckIcon, WarningIcon } from "@chakra-ui/icons";
-import { Button, Fade, Stack } from "@chakra-ui/react";
-import React from "react";
+import {
+	AlertDialog,
+	AlertDialogBody,
+	AlertDialogContent,
+	AlertDialogFooter,
+	AlertDialogHeader,
+	AlertDialogOverlay,
+	Button,
+	Fade,
+	Stack,
+	useDisclosure,
+} from "@chakra-ui/react";
+import React, { useRef } from "react";
 import { BeatLoader } from "react-spinners";
 
 interface IdleControlStatus {
@@ -30,10 +41,24 @@ interface ControlProps {
 	markSkipped: Control;
 }
 
+interface SkipDoseDialogProps {
+	isOpen: boolean;
+	close: () => void;
+	onConfirm: () => void;
+}
+
 interface ControlButtonProps {
 	color: string;
 	control: Control;
 }
+/**
+ * The status of an individual control
+ */
+export type ControlStatus =
+	| IdleControlStatus
+	| LoadingControlStatus
+	| CompleteControlStatus
+	| FailedControlStatus;
 
 const getControlIcon = (
 	status: ControlStatus
@@ -48,14 +73,50 @@ const getControlIcon = (
 	}
 };
 
-/**
- * The status of an individual control
- */
-export type ControlStatus =
-	| IdleControlStatus
-	| LoadingControlStatus
-	| CompleteControlStatus
-	| FailedControlStatus;
+const wrapControlAction = (
+	control: Control,
+	onAction: () => void
+): Control => ({
+	...control,
+	onAction,
+});
+
+const SkipDoseDialog = ({ isOpen, close, onConfirm }: SkipDoseDialogProps) => {
+	// Passing null is necessary here to satisfy the type checker
+	// https://github.com/chakra-ui/chakra-ui/discussions/2936
+	const cancelRef = useRef<HTMLButtonElement>(null);
+	const closeAndConfirm = () => {
+		close();
+		onConfirm();
+	};
+
+	const headerMsg = "Skip today?";
+	const confirmationMessage =
+		"Are you sure you want to skip today's medication? You will not receive any notifications for it today.";
+
+	return (
+		<AlertDialog
+			isCentered
+			leastDestructiveRef={cancelRef}
+			isOpen={isOpen}
+			onClose={close}
+		>
+			<AlertDialogOverlay />
+			<AlertDialogContent>
+				<AlertDialogHeader>{headerMsg}</AlertDialogHeader>
+				<AlertDialogBody>{confirmationMessage}</AlertDialogBody>
+				<AlertDialogFooter>
+					<Button ref={cancelRef} onClick={close}>
+						Cancel
+					</Button>
+					<Button colorScheme="red" marginLeft={3} onClick={closeAndConfirm}>
+						Skip
+					</Button>
+				</AlertDialogFooter>
+			</AlertDialogContent>
+		</AlertDialog>
+	);
+};
 
 const ControlButton = ({
 	color,
@@ -79,16 +140,36 @@ const ControlButton = ({
 	);
 };
 
-const Controls = (controls: ControlProps) => (
-	<Stack justifyContent="center">
-		<ControlButton color="teal" control={controls.markTaken}>
-			Mark taken
-		</ControlButton>
+const Controls = (controls: ControlProps) => {
+	const {
+		isOpen: isSkipDialogOpen,
+		onOpen: openSkipDialog,
+		onClose: closeSkipDialog,
+	} = useDisclosure();
 
-		<ControlButton color="red" control={controls.markSkipped}>
-			Skip today
-		</ControlButton>
-	</Stack>
-);
+	const confirmationSkipAction = wrapControlAction(
+		controls.markSkipped,
+		openSkipDialog
+	);
+
+	return (
+		<>
+			<Stack justifyContent="center">
+				<ControlButton color="teal" control={controls.markTaken}>
+					Mark taken
+				</ControlButton>
+
+				<ControlButton color="red" control={confirmationSkipAction}>
+					Skip today
+				</ControlButton>
+			</Stack>
+			<SkipDoseDialog
+				isOpen={isSkipDialogOpen}
+				close={closeSkipDialog}
+				onConfirm={controls.markSkipped.onAction}
+			/>
+		</>
+	);
+};
 
 export default Controls;
